02a93641c7d4f4c8c5e0bcaab8875857425cef3e
Lukas <LukasHoltbrink@web.de>
2023-12-23 12:35:45 +0100

    history

--- src/pages/Zoom.js
+++ src/pages/Zoom.js
@@ -3,14 +3,15 @@ import {useState, useEffect} from "react";
 import "./Zoom.css";
 
 
+
 function Zoom() {
 
 
 
 
   return (
-    <div>
-        <h1>hallo123</h1>
+    <div className='zoom'>
+      <img className='zoom_pic' src={process.env.PUBLIC_URL + '/img/pics/1.jpg'} />
     </div>
   )
 }
