51a510d5a02a9fa02099bf2545735a7b55e081b9
Lukas <LukasHoltbrink@web.de>
2024-02-02 14:49:57 +0100

    sortieren 2.2

--- src/App.js
+++ src/App.js
@@ -8,21 +8,26 @@ import Zoom from './pages/Zoom';
 import Nav from './Nav';
 import History from './pages/History';
 import { Route, Routes } from "react-router-dom"; 
+import {DndProvider} from "react-dnd";
+import {HTML5Backend} from "react-dnd-html5-backend"
 
 function App() {
   return (
-    <>
-    <div className="app">
-      <Routes>
-        <Route exact path='/' element={<HomeScreen />} />
-        <Route path='/celeb' element={<Celeb />} />
-        <Route path='/music' element={<Music />} />
-        <Route path='/geography' element={<Geography />} />
-        <Route path='/zoom' element={<Zoom />} />
-        <Route path='/history' element={<History />} />
-      </Routes>
-    </div>
-    </>
+    <DndProvider backend={HTML5Backend}>
+        <>
+        <div className="app">
+          <Routes>
+            <Route exact path='/' element={<HomeScreen />} />
+            <Route path='/celeb' element={<Celeb />} />
+            <Route path='/music' element={<Music />} />
+            <Route path='/geography' element={<Geography />} />
+            <Route path='/zoom' element={<Zoom />} />
+            <Route path='/history' element={<History />} />
+          </Routes>
+        </div>
+        </>
+    </DndProvider>
+   
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -11,13 +11,13 @@ const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(
-  <React.StrictMode>
+  
     <Provider store={store}>
       <BrowserRouter basename={process.env.PUBLIC_URL}>
         <App />
       </BrowserRouter>
     </Provider>
-  </React.StrictMode>
+  
 );
 
 // If you want to start measuring performance in your app, pass a function
