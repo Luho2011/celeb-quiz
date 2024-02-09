6a49830c651b869122c2cc2d60a63495a540aac1
Lukas <LukasHoltbrink@web.de>
2024-02-09 12:19:35 +0100

    9.2.24

--- src/pages/Movie.js
+++ src/pages/Movie.js
@@ -7,6 +7,8 @@ import list from "../movie.json"
 function Movie() {
   const [words, setWords] = useState(list);
   const [remainingWords, setRemainingWords] = useState([]);
+  const [coverImage, setCoverImage] = useState("");
+  const [showCover, setShowCover] = useState(true);
   const [currentIndex, setCurrentIndex] = useState(0);
 
   const handleNext = () => {
@@ -16,8 +18,11 @@ function Movie() {
   const handleStart = () => {
     const random = Math.floor(Math.random() * words.length);
     setRemainingWords(words[random].items);
+    const coverIt = words[random].items[0].cover;
+    setCoverImage(coverIt);
     setCurrentIndex(0);
     remove(words[random].ids);
+    setShowCover(true);
   };
 
   const remove = (id) => {
@@ -31,20 +36,23 @@ function Movie() {
       <div className='movie__buttons'>
           <button className='movie__button' onClick={handleStart}>start</button>
           <button className='movie__button' onClick={handleNext}>next</button>
+          <button className='movie__solutionButton' onClick={()=> setShowCover(!showCover)}>solution</button>       
       </div>
         <div className='movie__pics'>
-        <div className="image-container">
-          {remainingWords.map((item, index) => (
-            <div className="image-wrapper">
-              <img src={item.image} />
-              {currentIndex >= 1 && <img src={item.image2} />}
-              {currentIndex >= 2 && <img src={item.image3} />}
-              {currentIndex >= 3 && <img src={item.image4} />}
-            </div>
+        <div className={showCover ? "image-container" : "image-container-blur"}>
+          {remainingWords.map((item, index) => (  
+            <div className="image-wrapper">          
+                <img src={item.image} />
+                {currentIndex >= 1 && <img src={item.image2} />}
+                {currentIndex >= 2 && <img src={item.image3} />}
+                {currentIndex >= 3 && <img src={item.image4} />}
+              </div>
           ))}
         </div>
-        </div>
-          
+        </div>          
+              <div className={showCover ? "noShow" : "image__cover"}>
+                 <img src={coverImage} />
+              </div>              
     </div>
   )
 }
