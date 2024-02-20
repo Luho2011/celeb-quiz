7ae9d7d48ba6399652b0cfa607b73618dd80e358
Lukas <LukasHoltbrink@web.de>
2024-02-20 14:36:54 +0100

    20.2

--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,12 @@
+import {initializeApp} from 'firebase/app'
+
 const firebaseConfig = {
-    apiKey: "AIzaSyAeUY56bhO60AxEG0e6AfwfvpogOz36RUw",
-    authDomain: "quiz-4536a.firebaseapp.com",
-    projectId: "quiz-4536a",
-    storageBucket: "quiz-4536a.appspot.com",
-    messagingSenderId: "924204403595",
-    appId: "1:924204403595:web:8919e7e25aa1cc72403d8e"
-  };
+  apiKey: "AIzaSyBWynegWQ89QX3z_NY6jwGdEyQ5-pYMjAg",
+  authDomain: "quiz-469d1.firebaseapp.com",
+  projectId: "quiz-469d1",
+  storageBucket: "quiz-469d1.appspot.com",
+  messagingSenderId: "626084851156",
+  appId: "1:626084851156:web:3b73dccb66e1654cfafa41"
+};
+
+initializeApp(firebaseConfig);
