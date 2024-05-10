1abde4c3afd84f65e7b8353a24d2f25733d49f51
Lukas <LukasHoltbrink@web.de>
2024-05-10 17:20:31 +0200

    10.5

--- src/pages/Zoom.js
+++ src/pages/Zoom.js
@@ -56,7 +56,7 @@ function Zoom() {
   const getRandomNumber = () => {
     let rnd;
     do {
-      rnd = Math.floor(Math.random() * 44) + 1;
+      rnd = Math.floor(Math.random() * 46) + 1;
     } while (usedNumbers.includes(rnd));
     setUsedNumbers(prevState => [...prevState, rnd]);
     return rnd;
