8ce19c9e650c75a2f11acf6c48208001a8962ad0
Lukas <LukasHoltbrink@web.de>
2024-07-03 14:32:08 +0200

    3.7

--- src/pages/Movie2.js
+++ src/pages/Movie2.js
@@ -14,16 +14,21 @@ function Movie2() {
     e.currentTarget.classList.add('clicked');
   };
 
+  const isDifficult = (box) => {
+    return box.items.some((item) => item.difficulty === '2');
+  };
+ 
   return (
     <div className='movie2'>
       <div className='movie2__counter'>
        <Counter/>
       </div>
-      <h1>Choose one!</h1>
         <div className='movies2__all'>
           {movies.map((box) => (
             <Link to={`/${box.ids}`} target="_blank">
-              <button className='movie2Btns' key={box.ids} onClick={handleButtonClick}>
+              <button className={`movie2Btns ${isDifficult(box) ? "movie2Btns__difficult" : ""}`} key={box.ids} 
+              onClick={handleButtonClick}
+              >
                 <p>{box.ids}</p>
               </button> 
             </Link>
